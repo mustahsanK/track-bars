a929fc2a3734f356206e1cc5d2c6b02941470d4d
mustahsanK <mustahsan60@outlook.com>
2024-11-23 23:15:20 +0530

    added compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Compose dependencies
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.activity.ktx)
 }
